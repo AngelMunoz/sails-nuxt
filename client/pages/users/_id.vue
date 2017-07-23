346ed0ac2ffab43b3db40bc71c3988452da8377c
Angel D. Munoz <scythes.lol@gmail.com>
2017-07-23 10:28:22 -0500

    add user-form component and delete user action

--- client/pages/users/_id.vue
+++ client/pages/users/_id.vue
@@ -11,8 +11,7 @@
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn flat>Edit</v-btn>
-          <v-btn flat class="red--text">Delete</v-btn>
+          <v-btn v-on:click="deleteUser(user.id)" flat class="red--text">Delete</v-btn>
           <v-spacer></v-spacer>
           <v-btn icon @click.native="show = !show">
             <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
@@ -20,7 +19,7 @@
         </v-card-actions>
         <v-slide-y-transition>
           <v-card-text v-show="show">
-            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            <user-form v-bind:id="user.id" v-bind:name="user.name" v-bind:lastName="user.lastName" v-bind:username="user.username" v-on:updateduser="updateUser" />
           </v-card-text>
         </v-slide-y-transition>
       </v-card>
@@ -28,6 +27,7 @@
   </v-layout>
 </template>
 <script>
+import UserForm from '~components/UserForm.vue';
 export default {
   async asyncData({ params, store, app }) {
     let user;
@@ -41,13 +41,33 @@ export default {
       subtitle = user.name
       user.image = `https://randomuser.me/api/portraits/men/${user.id}.jpg`
     }
-    return { subtitle, user }
+    return { subtitle, user, editing: true }
   },
   data() {
     return {
       subtitle: 'Select from the list on the left',
       user: undefined,
-      show: false
+      show: false,
+      editing: false
+    }
+  },
+  components: {
+    UserForm
+  },
+  methods: {
+    updateUser({ username, name, lastName }) {
+      this.user.username = username;
+      this.user.name = name;
+      this.user.lastName = lastName;
+    },
+    async deleteUser(id) {
+      let result;
+      try {
+        result = await this.$axios.delete(`/users/${id}`).then(res => res.data);
+      } catch (error) {
+        return console.error(error);
+      }
+      this.$router.push({ name: 'users' })
     }
   }
 }
